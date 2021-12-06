106d1c26b0c2f64b68cb38854637fb32cc4b4b8a
momeppkt <peeranut32@gmail.com>
2021-12-06 22:02:59 +0700

    fix no remap keys

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -1,44 +1,44 @@
 local utils = require('utils')
 local remap = { noremap = false }
 
-utils.map('n', '<C-l>', '<cmd>nohl<cr>')                  -- clear highlight
-utils.map('n', 'dw', 'vb"_d')                             -- delete a word backward
-utils.map('n', '<leader>d', '"_d')                        -- delete without yank
+utils.map('n', '<C-l>', '<cmd>nohl<cr>')  -- clear highlight
+utils.map('n', 'dw', 'vb"_d')             -- delete a word backward
+utils.map('n', '<leader>d', '"_d')        -- delete without yank
 utils.map('n', 'x', '"_x')
-utils.map('v', '<leader>p', '"_dP')                       -- replace-paste without yank
-utils.map('i', '<S-Tab>', '<C-d>')                        -- de-tab while in insert mode
-utils.map('n', '<C-a>', 'gg<S-v>G', remap)                -- visual - select all
+utils.map('v', '<leader>p', '"_dP')       -- replace-paste without yank
+utils.map('i', '<S-Tab>', '<C-d>')        -- de-tab while in insert mode
+utils.map('n', '<C-a>', 'gg<S-v>G')       -- visual - select all
 
 -- Tab config
-utils.map('n', 'te', '<cmd>tabedit<cr>', remap)
-utils.map('n', 'tc', '<cmd>tabclose<cr>', remap)
-utils.map('n', '<Tab>', '<cmd>tabnext<cr>', remap)
-utils.map('n', '<S-Tab>', '<cmd>tabprev<cr>', remap)
+utils.map('n', 'te', '<cmd>tabedit<cr>')
+utils.map('n', 'tc', '<cmd>tabclose<cr>')
+utils.map('n', '<Tab>', '<cmd>tabnext<cr>')
+utils.map('n', '<S-Tab>', '<cmd>tabprev<cr>')
 
 -- Window config
 -- split window
-utils.map('n', 'ss', '<cmd>split<Return><C-w>w', remap)
-utils.map('n', 'sv', '<cmd>vsplit<Return><C-w>w', remap)
+utils.map('n', 'ss', '<cmd>split<Return><C-w>w')
+utils.map('n', 'sv', '<cmd>vsplit<Return><C-w>w')
 
 -- move window
-utils.map('n', 'sq', '<C-w>q', remap)
-utils.map('n', '<Space>', '<C-w>w', remap)
+utils.map('n', 'sq', '<C-w>q')
+utils.map('n', '<Space>', '<C-w>w')
 
-utils.map('', 's<left>', '<C-w>h', remap)
-utils.map('', 's<right>', '<C-w>l', remap)
-utils.map('', 's<up>', '<C-w>k', remap)
-utils.map('', 's<down>', '<C-w>j', remap)
+utils.map('', 's<left>', '<C-w>h')
+utils.map('', 's<right>', '<C-w>l')
+utils.map('', 's<up>', '<C-w>k')
+utils.map('', 's<down>', '<C-w>j')
 
-utils.map('', 'sh', '<C-w>h', remap)
-utils.map('', 'sl', '<C-w>l', remap)
-utils.map('', 'sk', '<C-w>k', remap)
-utils.map('', 'sj', '<C-w>j', remap)
+utils.map('', 'sh', '<C-w>h')
+utils.map('', 'sl', '<C-w>l')
+utils.map('', 'sk', '<C-w>k')
+utils.map('', 'sj', '<C-w>j')
 
 -- resize window
-utils.map('n', '<C-w><left>', '<C-w><', remap)
-utils.map('n', '<C-w><right>', '<C-w>>', remap)
-utils.map('n', '<C-w><up>', '<C-w>+', remap)
-utils.map('n', '<C-w><down>', '<C-w>-', remap)
+utils.map('n', '<C-w><left>', '<C-w><')
+utils.map('n', '<C-w><right>', '<C-w>>')
+utils.map('n', '<C-w><up>', '<C-w>+')
+utils.map('n', '<C-w><down>', '<C-w>-')
 
 -- move line or visually selected block - opt+j/k - must set iterm to esc+
 utils.map('i', '<m-j>', '<Esc><cmd>m .+1<CR>==gi')
