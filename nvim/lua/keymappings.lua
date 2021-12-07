f0899c6cf10ef84ed230bbf48e499ddbb3ab292d
momeppkt <peeranut32@gmail.com>
2021-12-07 15:28:12 +0700

    update keymaps and remove cmp key - fix lines move on visual mode - add more keys binding - remove <cr> key to accept completion

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -8,12 +8,32 @@ utils.map('n', 'x', '"_x')
 utils.map('v', '<leader>p', '"_dP')       -- replace-paste without yank
 utils.map('i', '<S-Tab>', '<C-d>')        -- de-tab while in insert mode
 utils.map('n', '<C-a>', 'gg<S-v>G')       -- visual - select all
+utils.map('n', 'Y', 'y$')                 -- Yank line after cursor
+utils.map('n', 'P', '<cmd>pu<cr>', remap) -- Paste on new line
+
+-- Keeping cursor centered
+utils.map('n', 'n', 'nzzzv')
+utils.map('n', 'N', 'Nzzzv')
+utils.map('n', 'J', 'mzJ`z')
+
+-- add undo break points
+utils.map('i', ',', ',<C-g>u')
+utils.map('i', '.', '.<C-g>u')
+utils.map('i', '!', '!<C-g>u')
+utils.map('i', '?', '?<C-g>u')
+
+-- Jumplist mutation
+utils.map('n', 'k', '(v:count > 5 ? "m\'" . v:count : "") . "k"', { noremap = true, expr = true })
+utils.map('n', 'j', '(v:count > 5 ? "m\'" . v:count : "") . "j"', { noremap = true, expr = true })
 
 -- Tab config
 utils.map('n', 'te', '<cmd>tabedit<cr>')
 utils.map('n', 'tc', '<cmd>tabclose<cr>')
 utils.map('n', '<Tab>', '<cmd>tabnext<cr>')
 utils.map('n', '<S-Tab>', '<cmd>tabprev<cr>')
+
+-- separate tab and ctrl-i -> Tab was bound to tabnext
+utils.map('n', '<C-i>', '<C-i>')
 
 -- Window config
 -- split window
@@ -41,20 +61,20 @@ utils.map('n', '<C-w><up>', '<C-w>+')
 utils.map('n', '<C-w><down>', '<C-w>-')
 
 -- move line or visually selected block - opt+j/k - must set iterm to esc+
-utils.map('i', '<m-j>', '<Esc><cmd>m .+1<CR>==gi')
-utils.map('i', '<m-k>', '<Esc><cmd>m .-2<CR>==gi')
-utils.map('i', '<m-down>', '<Esc><cmd>m .+1<CR>==gi')
-utils.map('i', '<m-up>', '<Esc><cmd>m .-2<CR>==gi')
+utils.map('i', '<m-j>', '<Esc>:m .+1<CR>==gi')
+utils.map('i', '<m-k>', '<Esc>:m .-2<CR>==gi')
+utils.map('i', '<m-down>', '<Esc>:m .+1<CR>==gi')
+utils.map('i', '<m-up>', '<Esc>:m .-2<CR>==gi')
 
-utils.map('v', '<m-j>', '<cmd>m ">+1<CR>gv=gv')
-utils.map('v', '<m-k>', '<cmd>m "<-2<CR>gv=gv')
-utils.map('v', '<m-down>', '<cmd>m ">+1<CR>gv=gv')
-utils.map('v', '<m-up>', '<cmd>m "<-2<CR>gv=gv')
+utils.map('v', '<m-k>', ':m \'<-2<CR>gv=gv')
+utils.map('v', '<m-j>', ':m \'>+1<CR>gv=gv')
+utils.map('v', '<m-down>', ':m \'>+1<CR>gv=gv')
+utils.map('v', '<m-up>', ':m \'<-2<CR>gv=gv')
 
-utils.map('n', '<m-j>', '<cmd>m+<CR>')
-utils.map('n', '<m-k>', '<cmd>m-2<CR>')
-utils.map('n', '<m-down>', '<cmd>m+<CR>')
-utils.map('n', '<m-up>', '<cmd>m-2<CR>')
+utils.map('n', '<m-j>', ':m+<CR>')
+utils.map('n', '<m-k>', ':m-2<CR>')
+utils.map('n', '<m-down>', ':m+<CR>')
+utils.map('n', '<m-up>', ':m-2<CR>')
 
 -- Terminal
 -- exit terminal to normal mode
