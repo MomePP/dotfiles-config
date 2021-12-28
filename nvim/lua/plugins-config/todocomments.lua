bfd67281943d16baf11bea45d80c0443b4a36527
momeppkt <peeranut32@gmail.com>
2021-12-28 14:58:23 +0700

    update `todocomment` colors and words recognize

--- nvim/lua/plugins-config/todocomments.lua
+++ nvim/lua/plugins-config/todocomments.lua
@@ -6,17 +6,13 @@ todocomments.setup {
   sign_priority = 8, -- sign priority
   -- keywords recognized as todo comments
   keywords = {
-    FIX = {
-      icon = " ", -- icon used for the sign, and in search results
-      color = "error", -- can be a hex color, or a named color (see below)
-      alt = { "fix", "error", "ERROR", "BUG", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-      -- signs = false, -- configure signs for some keywords individually
-    },
-    TODO = { icon = " ", color = "todo", alt = { "todo" } },
-    HACK = { icon = " ", color = "hack", alt = { "hack" } },
-    WARN = { icon = " ", color = "warning", alt = { "warn", "WARNING" } },
-    PERF = { icon = " ", color = "hint", alt = { "perf", "optimize", "PERFORMANCE", "OPTIMIZE" } },
-    NOTE = { icon = " ", color = "info", alt = { "note", "info", "INFO" } },
+    NOTE = { icon = " ", color = "note" },
+    TODO = { icon = " ", color = "todo" },
+    HACK = { icon = " ", color = "hack" },
+    WARN = { icon = " ", color = "warn", alt = { "WARNING" } },
+    INFO = { icon = " ", color = "info", alt = { "HINT" } },
+    PERF = { icon = " ", color = "hint", alt = { "OPTIMIZE" } },
+    FIX = { icon = " ", color = "error", alt = { "ERROR", "BUG", "ISSUE" } },
   },
   merge_keywords = true, -- when true, custom keywords will be merged with the defaults
   -- highlighting of the line containing the todo comment
@@ -35,13 +31,13 @@ todocomments.setup {
   -- list of named colors where we try to extract the guifg from the
   -- list of hilight groups or use the hex color if hl not found as a fallback
   colors = {
-    error = { "#D74E42", "DiagnosticError", "ErrorMsg" },
-    warning = { "#E9D16C", "DiagnosticWarning", "WarningMsg" },
-    info = {"#98C379", "DiagnosticInfo" },
-    hint = {  "#1085FF", "DiagnosticHint" },
+    error = { "#D74E42" },
+    warn = { "#E9D16C" },
+    note = { "#98C379" },
+    hint = { "#1085FF" },
+    info = { "#61AFEF" },
     todo = { "#B57EDC" },
     hack = { "#D19A66" },
-    default = { "Identifier" },
   },
   search = {
     command = "rg",
