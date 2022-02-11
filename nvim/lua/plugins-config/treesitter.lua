6c23ea1bf65d3b63d0e7f255158da8107667fa11
momeppkt <peeranut32@gmail.com>
2022-02-11 16:41:56 +0700

    update `treesitter` ignore list

--- nvim/lua/plugins-config/treesitter.lua
+++ nvim/lua/plugins-config/treesitter.lua
@@ -2,29 +2,32 @@ local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status_ok then return end
 
 treesitter.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-    additional_vim_regex_highlighting = true,
-  },
-  indent = {
-    enable = false,
-    disable = { 'yaml' },
-  },
-  ensure_installed = "maintained",
-  sync_install = false,
-  ignore_install = { 'norg' },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    colors = {
-      "#ffd700",
-      "#da70d6",
-      "#87cefa"
-    }, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  }
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = true,
+    },
+    indent = {
+        enable = false,
+        disable = { 'yaml' },
+    },
+    ensure_installed = "maintained",
+    sync_install = false,
+    ignore_install = {
+        'norg',
+        'vala'
+    },
+    rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        colors = {
+            "#ffd700",
+            "#da70d6",
+            "#87cefa"
+        }, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
+    }
 }
 
