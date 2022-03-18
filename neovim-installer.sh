aad84339f399ff0826dd9b1c342599c98f377a7c
momeppkt <peeranut32@gmail.com>
2022-03-18 14:00:19 +0700

    feat(lazygit): update `lazygit` config as a symlink, instead of creating config file

--- neovim-installer.sh
+++ neovim-installer.sh
@@ -7,24 +7,6 @@ brew install wget neovim lazygit git-flow ripgrep neovim-remote gotop fd
 # currently handles by using lazygit config -> the downside, it cannot use lazygit outside nvim
 # echo -e "export VISUAL=\"nvr --remote-wait + 'set bufhidden=wipe'\"\n" >> ~/.config/fish/config.fish
 
-# -- add edit and open commands for lazygit config if config not exists
-lazygit_config=~/Library/Application\ Support/lazygit/config.yml
-update_lazygit_config="y"
-found_lazygit_config=false
-if [ -f "$lazygit_config" ]; then
-    found_lazygit_config=true
-    read -p "found exist lazygit config file.. overwrite (y) or (n) ? : " update_lazygit_config
-fi
-if [ $update_lazygit_config = "y" ]; then
-    if $found_lazygit_config; then
-        rm "$lazygit_config"
-    fi
-    echo -e "os:\n  editCommand: \"nvim\"\n  openCommand: \"nvr --remote-silent -cc q {{filename}}\"" >> "$lazygit_config"
-    echo "added lazygit config !"
-else
-    echo "skipped lazygit config.."
-fi
-
 # -- move nvim config files to local config directory
 config_path=~/.config
 update_nvim_config="y"
@@ -41,5 +23,23 @@ if [ $update_nvim_config = "y" ]; then
     echo "added neovim config !"
 else
     echo "skipped neovim config.."
+fi
+
+# -- add edit and open commands for lazygit config if config not exists
+lazygit_config=~/Library/Application\ Support/lazygit/config.yml
+update_lazygit_config="y"
+found_lazygit_config=false
+if [[ -f "$lazygit_config" || -L "$lazygit_config" ]]; then
+    found_lazygit_config=true
+    read -p "found exist lazygit config.. overwrite (y) or (n) ? : " update_lazygit_config
+fi
+if [ $update_lazygit_config = "y" ]; then
+    if $found_lazygit_config; then
+        rm "$lazygit_config" # remove old symlink or old config file
+    fi
+    ln -s "${config_path}/lazygit/config.yml" "$lazygit_config"
+    echo "added lazygit config !"
+else
+    echo "skipped lazygit config.."
 fi
 
