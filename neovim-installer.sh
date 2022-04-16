c4ae53c46f6181932c00a78fe8971b1fb0866a1c
momeppkt <peeranut32@gmail.com>
2022-04-16 18:33:48 +0700

    feat(nvim remote): replace `neovim-remote` with native remote flag - remove `neovim-remote` or `nvr` from installer deps script - change lazygit `openCommand` to used `neovim --remote`, currently   need to send key to close lazygit at first then open file in new tab

--- neovim-installer.sh
+++ neovim-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # WARN: must have brew installed
-brew install wget neovim lazygit git-flow ripgrep neovim-remote gotop fd
+brew install wget neovim lazygit git-flow ripgrep gotop fd
 
 # NOTE: add neovim-remote config to default shell config (currently using fish)
 # currently handles by using lazygit config -> the downside, it cannot use lazygit outside nvim
