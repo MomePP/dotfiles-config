50ef21d7809a3ba9443f2e7cb613d9e4e010f4ee
momeppkt <peeranut32@gmail.com>
2022-04-20 15:04:17 +0700

    try to avoid tabpage usage from `bufferline`

--- nvim/lua/plugins-config/bufferline.lua
+++ nvim/lua/plugins-config/bufferline.lua
@@ -3,6 +3,7 @@ if not status_ok then return end
 
 bufferline.setup {
     options = {
+        mode = 'buffers',
         numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
         close_command = "Bdelete! %d",       -- can be a string | function, see "Mouse actions"
         right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
