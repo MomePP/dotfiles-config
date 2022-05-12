d4bcc4912495b1cbf380310d2bf38f79cfc8eef2
momeppkt <peeranut32@gmail.com>
2022-05-12 17:24:47 +0700

    update `staline` colors

--- nvim/lua/plugins-config/auto-session.lua
+++ nvim/lua/plugins-config/auto-session.lua
@@ -3,6 +3,8 @@ if not status_ok then return end
 
 autosession.setup {
     log_level = 'error',
+    auto_save_enabled = true,
+    auto_restore_enabled = true,
     auto_session_suppress_dirs = {
         '~/',
     },
--- nvim/lua/plugins-config/staline.lua
+++ nvim/lua/plugins-config/staline.lua
@@ -2,16 +2,16 @@ local status_sta, staline = pcall(require, 'staline')
 if not status_sta then return end
 
 local colors = {
-    white        = '#eeeeee',
+    white        = '#e4e4e4',
     red          = '#E06C75',
     green        = '#8fbf7f',
     olive        = '#84a598',
     blue         = '#0087ff',
     cream        = '#E6CCA9',
-    yellow       = '#E9D16C',
+    yellow       = '#E5C07B',
     purple       = '#af5fff',
     orange       = '#fc802d',
-    gray         = '#5f6672',
+    gray         = '#a8a8a8',
     darkgray     = '#21252b',
     transparent  = 'NONE',
 }
@@ -20,14 +20,13 @@ staline.setup {
     sections = {
         left = {
             ' ',
-            'mode',
+            { 'StalineMode', 'mode' },
             ' ',
             'branch',
             '  ',
             function ()
                 local session_name = 'none'
-                local session_path = vim.v.this_session
-                if session_path ~= '' then
+                if vim.v.this_session ~= '' then
                     session_name = require('auto-session-library').current_session_name()
                 end
                 return "神" .. session_name
@@ -36,23 +35,23 @@ staline.setup {
         mid = {
             'file_name',
             '',
-            'file_size'
+            { 'StalineFileSize', 'file_size' }
         },
         right = {
             '-lsp',
             ' ',
-            'lsp_name',
+            { 'StalineLSP', 'lsp_name' },
             ' ',
             'line_column'
         }
 	},
 	mode_colors = {
-		i = colors.green,
-		n = colors.cream,
-		c = colors.red,
-		v = colors.purple,
-        V = colors.orange,
-        t = colors.blue,
+		i = colors.gray,
+		n = colors.white,
+		c = colors.gray,
+		v = colors.gray,
+        V = colors.gray,
+        t = colors.gray,
 	},
     mode_icons = {
 		n = " ",
@@ -62,11 +61,17 @@ staline.setup {
 		true_colors = true,
 		line_column = " [%l/%L] :%c",
 		branch_symbol = " ",
+        bg = colors.darkgray,
 	},
     special_table = {
         toggleterm = { 'ToggleTerm', ' ' }
     }
 }
+
+vim.cmd("hi StalineMode guifg=" .. colors.purple .. " guibg=" .. colors.darkgray)
+vim.cmd("hi StalineSession guifg=" .. colors.purple .. " guibg=" .. colors.darkgray)
+vim.cmd("hi StalineFileSize guifg=" .. colors.purple .. " guibg=" .. colors.darkgray)
+vim.cmd("hi StalineLSP guifg=" .. colors.purple .. " guibg=" .. colors.darkgray)
 
 -- require'stabline'.setup {
 --     style       = "bar", -- others: arrow, slant, bubble
