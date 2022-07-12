c2bcd6f3c435b01c97ecb1b585e3d3ce0a2cb3d4
momeppkt <peeranut32@gmail.com>
2022-07-12 16:45:33 +0700

    update autosave only in session, need to manually create session

--- nvim/lua/plugins-config/session-manager-config.lua
+++ nvim/lua/plugins-config/session-manager-config.lua
@@ -4,8 +4,5 @@ if not status_ok then return end
 session_manager.setup({
     autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
     autosave_ignore_not_normal = true,
-    autosave_ignore_filetypes = {
-        'gitcommit',
-        '', -- ignore [NO NAME] buffer
-    }
+    autosave_only_in_session = true,
 })
