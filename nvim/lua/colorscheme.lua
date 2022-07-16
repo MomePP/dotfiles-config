5d0b54e67c43bbea9bcb15555432290d370b5a95
momeppkt <peeranut32@gmail.com>
2022-07-16 17:38:53 +0700

    feat(nvim): fix terminal colors was overrided by `kanagawa` colorscheme

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -1,16 +1,10 @@
 -- vim.g.plastic_nvim_transparent_bg = true
 -- vim.cmd('colorscheme plastic-nvim')
 
-local kanagawa_colors = require('kanagawa.colors').setup()
-local overrided_hlgroup = {
-    VertSplit = { fg = kanagawa_colors.sumiInk2, bg = 'NONE' },
-    StatusLineNC = { bg = 'NONE' },
-}
-
 require('kanagawa').setup({
-    undercurl = true, -- enable undercurls
     transparent = true, -- do not set background color
-    overrides = overrided_hlgroup,
+    globalStatus = true,
+    terminalColors = false,
 })
 vim.cmd('colorscheme kanagawa')
 
