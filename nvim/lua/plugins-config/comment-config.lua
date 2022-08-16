0d3ce26555e5018adcaddce8a53c10da860bf446
momeppkt <peeranut32@gmail.com>
2022-08-16 12:58:53 +0700

    fix(nvim-plugin): fix `Comment.nvim` breaking change with `U.ctype` affected `nvim-ts-context-commentstring`

--- nvim/lua/plugins-config/comment-config.lua
+++ nvim/lua/plugins-config/comment-config.lua
@@ -3,19 +3,20 @@ if not status_ok then return end
 
 comment.setup {
     pre_hook = function(ctx)
-        local U = require 'Comment.utils'
+        local U = require('Comment.utils')
 
+        -- Determine whether to use linewise or blockwise commentstring
+        local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
+
+        -- Determine the location where to calculate commentstring from
         local location = nil
-        if ctx.ctype == U.ctype.block then
+        if ctx.ctype == U.ctype.blockwise then
             location = require('ts_context_commentstring.utils').get_cursor_location()
         elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
             location = require('ts_context_commentstring.utils').get_visual_start_location()
         end
 
-        return require('ts_context_commentstring.internal').calculate_commentstring {
-            key = ctx.ctype == U.ctype.line and '__default' or '__multiline',
-            location = location,
-        }
+        return require('ts_context_commentstring.internal').calculate_commentstring({ key = type, location = location, })
     end,
 }
 
@@ -25,6 +26,5 @@ vim.bo.commentstring = '//%s'
 local ft = require('Comment.ft')
 
 -- Multiple filetypes
-ft({'json', 'rust'}, {'//%s', '/*%s*/'})
-ft({'toml', 'graphql'}, '#%s')
-
+ft({ 'json', 'rust' }, { '//%s', '/*%s*/' })
+ft({ 'toml', 'graphql' }, '#%s')
