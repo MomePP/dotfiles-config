023a1114a114c3220a29580313043f5c64aca648
momeppkt <peeranut32@gmail.com>
2022-10-04 13:54:46 +0700

    fix(nvim-plugin): remove current bugfix, due to conflicts with telescope actions

--- nvim/lua/plugins-config/focus-config.lua
+++ nvim/lua/plugins-config/focus-config.lua
@@ -12,7 +12,7 @@ local focus_configs = {
         'terminal'
     },
     excluded_windows = {},
-    autoresize = false,
+    autoresize = true,
     number = false,
     signcolumn = false,
 }
@@ -21,16 +21,16 @@ focus.setup(focus_configs)
 
 -- INFO: changed user autocmd to prevents resized notifier's window
 -- INFO:    this need `notifier` to triggers autocmd by remove `noautocmd` option when open new window
-local notifier_ok, notifier_status_module = pcall(require, 'notifier.status')
-vim.api.nvim_create_autocmd('BufEnter', {
-    desc = '`focus.nvim` resized windows',
-    pattern = '*',
-    callback = function()
-        if notifier_ok and notifier_status_module.win_nr ~= nil then
-            table.insert(focus_configs.excluded_windows, notifier_status_module.win_nr)
-            focus.setup(focus_configs)
-        end
-        vim.api.nvim_exec_autocmds('WinScrolled', {})
-        focus.resize()
-    end
-})
+-- local notifier_ok, notifier_status_module = pcall(require, 'notifier.status')
+-- vim.api.nvim_create_autocmd('BufEnter', {
+--     desc = '`focus.nvim` resized windows',
+--     pattern = '*',
+--     callback = function()
+--         if notifier_ok and notifier_status_module.win_nr ~= nil then
+--             table.insert(focus_configs.excluded_windows, notifier_status_module.win_nr)
+--             focus.setup(focus_configs)
+--         end
+--         vim.api.nvim_exec_autocmds('WinScrolled', {})
+--         focus.resize()
+--     end
+-- })
