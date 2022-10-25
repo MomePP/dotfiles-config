4f587e4ab4b97e339e6601cc0863fe3dbf2c7ee0
momeppkt <peeranut32@gmail.com>
2022-10-25 15:38:21 +0700

    fix(nvim-lsp-setting): update tsdk path for volar lsp

--- nvim/lua/lsp-config/settings/volar.lua
+++ nvim/lua/lsp-config/settings/volar.lua
@@ -1,7 +1,7 @@
 return {
     init_options = {
         typescript = {
-            tsdk = '/opt/homebrew/lib/node_modules/typescript/lib'
+            tsdk = '~/.local/share/nvm/v19.0.0/lib/node_modules/typescript/lib',
         }
     }
 }
