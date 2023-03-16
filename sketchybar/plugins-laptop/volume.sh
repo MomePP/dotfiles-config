977cbe571ea2bc105bc4459f966074354a32761a
momeppkt <peeranut32@gmail.com>
2023-03-17 00:16:19 +0700

    fix(sketchybar): fix volume icon padding and show volume level

--- sketchybar/plugins-laptop/volume.sh
+++ sketchybar/plugins-laptop/volume.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env sh
 
-case ${INFO} in
+VOLUME=$INFO
+
+case $VOLUME in
   [6-9][0-9]|100)
-    ICON=""
+    ICON="墳"
     ICON_PADDING_RIGHT=5
-    ;;
-  [1-5][0-9]|[1-9])
-    ICON=""
+  ;;
+  [3-5][0-9])
+    ICON="奔"
+    ICON_PADDING_RIGHT=8
+  ;;
+  [1-2][0-9]) 
+    ICON=""
     ICON_PADDING_RIGHT=11
-    ;;
-  *) ICON=""
-    ICON_PADDING_RIGHT=15
+  ;;
+  [1-9])
+    ICON=""
+    ICON_PADDING_RIGHT=18
+  ;;
+  *) 
+    ICON="婢"
+    ICON_PADDING_RIGHT=12
 esac
 
-sketchybar --set $NAME icon=$ICON icon.padding_right=$ICON_PADDING_RIGHT
+sketchybar --set $NAME icon="$ICON" icon.padding_right=$ICON_PADDING_RIGHT label="$VOLUME%"
