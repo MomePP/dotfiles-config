ac9aba1994877d385714ac88dd35d26a0523b83b
momeppkt <peeranut32@gmail.com>
2023-04-17 18:17:19 +0700

    fix(nvim/trouble): fix lazy load event to not loaded from config itself

--- nvim/lua/plugins/trouble-config.lua
+++ nvim/lua/plugins/trouble-config.lua
@@ -1,27 +1,26 @@
 local M = {
     'folke/trouble.nvim',
     cmd = 'TroubleToggle',
-    event = 'VeryLazy'
 }
 
-M.init = function()
+M.opts = function()
     -- INFO: extends action to close and focus back on active split
     local trouble = require('trouble')
     trouble.close_to_parent = function()
         trouble.action('cancel')
         trouble.close()
     end
-end
 
-M.opts = {
-    use_diagnostic_signs = true,
-    action_keys = {
-        close = {},
-        cancel = {},
-        close_to_parent = { 'q', '<esc>' },
-        jump = { '<tab>', '<space>' },
-        jump_close = { '<cr>', 'o' }
+    return {
+        use_diagnostic_signs = true,
+        action_keys = {
+            close = {},
+            cancel = {},
+            close_to_parent = { 'q', '<esc>' },
+            jump = { '<tab>', '<space>' },
+            jump_close = { '<cr>', 'o' }
+        }
     }
-}
+end
 
 return M
