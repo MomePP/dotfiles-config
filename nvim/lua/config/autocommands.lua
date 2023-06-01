3ce723221dfea812ed871dee56e5503ab3d71e90
momeppkt <peeranut32@gmail.com>
2023-06-01 15:30:45 +0700

    fix(nvim/vimdiff): set keymap to quit vimdiff mode from OptionSet event

--- nvim/lua/config/autocommands.lua
+++ nvim/lua/config/autocommands.lua
@@ -16,6 +16,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function() vim.highlight.on_yank({ on_visual = false }) end
 })
 
+vim.api.nvim_create_autocmd('OptionSet', {
+    desc = 'assign q to quit when in diff mode',
+    pattern = 'diff',
+    callback = function () vim.keymap.set('n', 'q', '<c-w>h:q<cr>', { buffer = true }) end
+})
+
 -- check if we need to reload file when it changed
 vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
     command = 'checktime'
