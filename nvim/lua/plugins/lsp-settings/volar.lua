75fcc5d339619e9a54091f8cc44bfc74165b2338
momeppkt <peeranut32@gmail.com>
2023-07-16 19:33:37 +0700

    fix(nvim/lsp): fix volar typescript to use the global one

--- nvim/lua/plugins/lsp-settings/volar.lua
+++ nvim/lua/plugins/lsp-settings/volar.lua
@@ -1,3 +1,8 @@
 return {
-    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    init_options = {
+        typescript = {
+            tsdk = '/opt/homebrew/lib/node_modules/typescript/lib'
+        }
+    }
 }
