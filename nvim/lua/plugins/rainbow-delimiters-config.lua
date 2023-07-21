47171ec39f50fabee8cb2042eb61313e4eab2246
momeppkt <peeranut32@gmail.com>
2023-07-21 07:42:38 +0700

    chore(nvim/rainbow-delimiter): use default strategy instead of user configured

--- nvim/lua/plugins/rainbow-delimiters-config.lua
+++ nvim/lua/plugins/rainbow-delimiters-config.lua
@@ -3,12 +3,7 @@ local M = {
 }
 
 M.config = function()
-    local rainbow_delimiters = require('rainbow-delimiters')
     vim.g.rainbow_delimiters = {
-        strategy = {
-            [''] = rainbow_delimiters.strategy['global'],
-            commonlisp = rainbow_delimiters.strategy['local'],
-        },
         query = {
             [''] = 'rainbow-delimiters',
             lua = 'rainbow-blocks',
