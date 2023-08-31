1cab944a60562869616d574a854399d70a0a634c
momeppkt <peeranut32@gmail.com>
2023-08-31 13:34:27 +0700

    perf(nvim/colorizer): enable on only allowed filetypes

--- nvim/lua/plugins/colorizer-config.lua
+++ nvim/lua/plugins/colorizer-config.lua
@@ -5,7 +5,7 @@ local M = {
 
 M.opts = {
     user_default_options = {
-        RGB = false,          -- #RGB hex codes
+        RGB = false,         -- #RGB hex codes
         RRGGBB = true,       -- #RRGGBB hex codes
         RRGGBBAA = true,     -- #RRGGBBAA hex codes
         AARRGGBB = false,    -- 0xAARRGGBB hex codes
@@ -14,10 +14,19 @@ M.opts = {
         hsl_fn = false,      -- CSS hsl() and hsla() functions
         css = false,         -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = false,      -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes for `mode`: foreground, background,  virtualtext
         mode = 'background', -- Set the display mode.
         virtualtext = '■',
-    }
+    },
+    filetypes = {
+        'html',
+        'css',
+        'scss',
+        'javascript',
+        'typescript',
+        'tsx',
+        'vue',
+        'svelte'
+    },
 }
 
 return M
