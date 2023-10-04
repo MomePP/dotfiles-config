f79a06168222e3ce905a755f94620ca508bcff40
momeppkt <peeranut32@gmail.com>
2023-10-04 11:44:51 +0700

    fix(sketchybar): workaround for `wifi_change` event not working on sonoma

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -2,8 +2,8 @@
 
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
-
-if [ "$SENDER" = "wifi_change" ]; then
+# if [ "$SENDER" = "wifi_change" ]; then
+    INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
     if [[ -z "$INFO" ]]; then
         ICON="󰤫"
     else
@@ -12,4 +12,4 @@ if [ "$SENDER" = "wifi_change" ]; then
     WIFI=${INFO:-"Not Connected"}
 
     sketchybar --set $NAME label="${WIFI}" icon="${ICON}"
-fi
+# fi
