ebc46d71bae8f369481a98514a8064a562eb0dd6
momeppkt <peeranut32@gmail.com>
2023-10-07 13:39:57 +0700

    chore(nvim/focus): cleanup unnecessary autocmd to disable focus.nvim

--- nvim/lua/plugins/focus-config.lua
+++ nvim/lua/plugins/focus-config.lua
@@ -3,35 +3,6 @@ local M = {
     event = 'UIEnter'
 }
 
-M.init = function()
-    local ignore_filetypes = { 'toggleterm' }
-    local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
-
-    local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
-    vim.api.nvim_create_autocmd('WinEnter', {
-        group = augroup,
-        callback = function(_)
-            if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-                vim.w.focus_disable = true
-            else
-                vim.w.focus_disable = false
-            end
-        end,
-        desc = 'Disable focus autoresize for BufType',
-    })
-    vim.api.nvim_create_autocmd('FileType', {
-        group = augroup,
-        callback = function(_)
-            if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-                vim.w.focus_disable = true
-            else
-                vim.w.focus_disable = false
-            end
-        end,
-        desc = 'Disable focus autoresize for FileType',
-    })
-end
-
 M.opts = {
     ui = { signcolumn = false }
 }
