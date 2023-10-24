3988b6c3c7b40b16f4afd5a3ab1e4f0acbc8164a
momeppkt <peeranut32@gmail.com>
2023-10-24 09:38:14 +0700

    fix(nvim/focus): restore ignore buftype, don't resize nofile

--- nvim/lua/plugins/focus-config.lua
+++ nvim/lua/plugins/focus-config.lua
@@ -3,6 +3,24 @@ local M = {
     event = 'UIEnter'
 }
 
+M.init = function()
+    local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+    local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+    vim.api.nvim_create_autocmd('WinEnter', {
+        group = augroup,
+        callback = function(_)
+            if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+            then
+                vim.w.focus_disable = true
+            else
+                vim.w.focus_disable = false
+            end
+        end,
+        desc = 'Disable focus autoresize for BufType',
+    })
+end
+
 M.opts = {
     ui = { signcolumn = false }
 }
