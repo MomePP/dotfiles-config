a03e914b051756c8a3eeaf5d74ce22588fd041dd
momeppkt <peeranut32@gmail.com>
2023-10-30 16:07:00 +0700

    feat(sketchybar): use `max_chars` to truncate label and enable text scrolling

--- sketchybar/plugins/media_info.sh
+++ sketchybar/plugins/media_info.sh
@@ -28,21 +28,21 @@ update_track() {
     TRACK_LENGTH=${#TRACK}
     ARTIST_LENGTH=${#ARTIST}
 
-    if [ $((TRACK_LENGTH + ARTIST_LENGTH)) -gt $MAX_LENGTH ]; then
-      # If the total length exceeds the max
-      if [ $TRACK_LENGTH -gt $HALF_LENGTH ] && [ $ARTIST_LENGTH -gt $HALF_LENGTH ]; then
-        # If both the track and artist are too long, cut both at half length - 1
-        # If MAX_LENGTH is odd, HALF_LENGTH is calculated with an extra space, so give it an extra char
-        TRACK="${TRACK:0:$((MAX_LENGTH % 2 == 0 ? HALF_LENGTH - 2 : HALF_LENGTH - 1))}…"
-        ARTIST="${ARTIST:0:$((HALF_LENGTH - 2))}…"
+    # if [ $((TRACK_LENGTH + ARTIST_LENGTH)) -gt $MAX_LENGTH ]; then
+    #   # If the total length exceeds the max
+    #   if [ $TRACK_LENGTH -gt $HALF_LENGTH ] && [ $ARTIST_LENGTH -gt $HALF_LENGTH ]; then
+    #     # If both the track and artist are too long, cut both at half length - 1
+    #     # If MAX_LENGTH is odd, HALF_LENGTH is calculated with an extra space, so give it an extra char
+    #     TRACK="${TRACK:0:$((MAX_LENGTH % 2 == 0 ? HALF_LENGTH - 2 : HALF_LENGTH - 1))}…"
+    #     ARTIST="${ARTIST:0:$((HALF_LENGTH - 2))}…"
 
-      elif [ $TRACK_LENGTH -gt $HALF_LENGTH ]; then
-        # Else if only the track is too long, cut it by the difference of the max length and artist length
-        TRACK="${TRACK:0:$((MAX_LENGTH - ARTIST_LENGTH - 1))}…"
-      elif [ $ARTIST_LENGTH -gt $HALF_LENGTH ]; then
-        ARTIST="${ARTIST:0:$((MAX_LENGTH - TRACK_LENGTH - 1))}…"
-      fi
-    fi
+    #   elif [ $TRACK_LENGTH -gt $HALF_LENGTH ]; then
+    #     # Else if only the track is too long, cut it by the difference of the max length and artist length
+    #     TRACK="${TRACK:0:$((MAX_LENGTH - ARTIST_LENGTH - 1))}…"
+    #   elif [ $ARTIST_LENGTH -gt $HALF_LENGTH ]; then
+    #     ARTIST="${ARTIST:0:$((MAX_LENGTH - TRACK_LENGTH - 1))}…"
+    #   fi
+    # fi
     sketchybar --set $NAME label="${TRACK}  ${ARTIST}" label.drawing=yes icon.color=$GREEN
 
   elif [ "$PLAYER_STATE" = "paused" ]; then
