0971e98d83ce4a63ec9a73b4042ef9860ac2ece3
momeppkt <peeranut32@gmail.com>
2023-11-01 11:25:40 +0700

    fix(nvim/move): fix keymaps was not set to plugin spec.

--- nvim/lua/plugins/move-config.lua
+++ nvim/lua/plugins/move-config.lua
@@ -1,18 +1,29 @@
+local move_keymap = require('config.keymaps').move
+
 local M = {
     'echasnovski/mini.move',
     main = 'mini.move',
-    config = true,
 }
 
-M.keys = function()
-    local move_keymap = require('config.keymaps').move
+M.opts = {
+    mappings = {
+        left = move_keymap.move_left,
+        right = move_keymap.move_right,
+        down = move_keymap.move_down,
+        up = move_keymap.move_up,
 
-    return {
-        { move_keymap.move_up,    mode = { 'n', 'x' } },
-        { move_keymap.move_down,  mode = { 'n', 'x' } },
-        { move_keymap.move_left,  mode = { 'n', 'x' } },
-        { move_keymap.move_right, mode = { 'n', 'x' } },
+        line_left = move_keymap.move_left,
+        line_right = move_keymap.move_right,
+        line_down = move_keymap.move_down,
+        line_up = move_keymap.move_up,
     }
-end
+}
+
+M.keys = {
+    { move_keymap.move_up,    mode = { 'n', 'x' } },
+    { move_keymap.move_down,  mode = { 'n', 'x' } },
+    { move_keymap.move_left,  mode = { 'n', 'x' } },
+    { move_keymap.move_right, mode = { 'n', 'x' } },
+}
 
 return M
