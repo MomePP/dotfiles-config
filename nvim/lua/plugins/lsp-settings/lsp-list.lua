735821b7ae66720d278f2f333f3e3e022ade94b1
momeppkt <peeranut32@gmail.com>
2023-11-09 18:25:59 +0700

    feat(nvim/pyright): add pyright config to ensure diagnostic only enabled with opened files

--- nvim/lua/plugins/lsp-settings/lsp-list.lua
+++ nvim/lua/plugins/lsp-settings/lsp-list.lua
@@ -2,6 +2,7 @@ return {
     clangd = require('plugins.lsp-settings.clangd'),
     lua_ls = require('plugins.lsp-settings.lua-ls'),
     volar = require('plugins.lsp-settings.volar'),
+    pyright = require('plugins.lsp-settings.pyright'),
     marksman = {},
     rust_analyzer = {},
     jsonls = {},
