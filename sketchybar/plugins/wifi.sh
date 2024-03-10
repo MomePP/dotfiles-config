04ab5b547afa3409b27eb812b7e088f8ab6064cf
momeppkt <peeranut32@gmail.com>
2024-03-10 16:16:10 +0700

    fix(sketchybar): fix getting ssid name from `airport` command has been deprecated

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,7 +3,7 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 if [ "$SENDER" = "wifi_change" ]; then
-    INFO="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
+    INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed 's/Current Wi-Fi Network: //')"
     if [[ -z "$INFO" ]]; then
         ICON="󰤫"
     else
