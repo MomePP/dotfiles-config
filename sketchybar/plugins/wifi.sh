e7dc5bdc4596a99d17926818550a2318b6eaccc8
momeppkt <peeranut32@gmail.com>
2024-03-17 13:29:19 +0700

    fix(sketchybar): update wifi script to trim whitespaces and handles not connected msg

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,13 +3,13 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 if [ "$SENDER" = "wifi_change" ]; then
-    INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed 's/Current Wi-Fi Network: //')"
-    if [[ -z "$INFO" ]]; then
+    INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
+    if [[ -z "$INFO" ]] || [[ -z "${INFO##You are not*}" ]]; then
         ICON="󰤫"
+        WIFI="Not Connected"
     else
         ICON="󰤨"
+        WIFI=$(echo $INFO | tr -d '\t')
     fi
-    WIFI=${INFO:-"Not Connected"}
-
     sketchybar --set $NAME label="${WIFI}" icon="${ICON}"
 fi
