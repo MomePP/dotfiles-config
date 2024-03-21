f53ac65434317b5e8ac7f0a659ed14788c28c3ed
momeppkt <peeranut32@gmail.com>
2024-03-21 10:58:14 +0700

    feat(nvim/lsp): change python lsp to `basedpyright`

---
+++ nvim/lua/plugins/lsp-settings/basedpyright.lua
@@ -0,0 +1,13 @@
+return {
+    settings = {
+        basedpyright = {
+            typeCheckingMode = 'standard',
+            analysis = {
+                autoImportCompletions = true,
+                autoSearchPaths = true,
+                diagnosticMode = "openFilesOnly",
+                useLibraryCodeForTypes = true
+            }
+        }
+    }
+}
--- nvim/lua/plugins/lsp-settings/lsp-list.lua
+++ nvim/lua/plugins/lsp-settings/lsp-list.lua
@@ -2,7 +2,7 @@ return {
     clangd = require('plugins.lsp-settings.clangd'),
     lua_ls = require('plugins.lsp-settings.lua-ls'),
     volar = require('plugins.lsp-settings.volar'),
-    pyright = require('plugins.lsp-settings.pyright'),
+    basedpyright = require('plugins.lsp-settings.basedpyright'),
     marksman = {},
     rust_analyzer = {},
     jsonls = {},
