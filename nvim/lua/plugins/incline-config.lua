f381aa859e451de18883256ff918e5afe0ff4118
momeppkt <peeranut32@gmail.com>
2024-03-24 17:17:34 +0700

    feat(nvim/incline): update filename to show path dynamically

--- nvim/lua/plugins/incline-config.lua
+++ nvim/lua/plugins/incline-config.lua
@@ -5,7 +5,7 @@ local M = {
 }
 
 M.opts = function()
-    -- local helpers = require 'incline.helpers'
+    local truncate_utils = require('plenary.strings').truncate
 
     return {
         debounce_threshold = {
@@ -18,15 +18,32 @@ M.opts = function()
             zindex = 10,
         },
         render = function(props)
-            local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-            local ft_icon, ft_color = require('nvim-web-devicons').get_icon_color(filename)
-            local buffer = {
-                ' ',
-                ft_icon and { ft_icon, guifg = ft_color } or '',
-                ' ',
-                { filename, gui = 'bold', guifg = vim.bo[props.buf].modified and '#ffaa00' or nil },
+            local bufname = vim.api.nvim_buf_get_name(props.buf)
+            local win_width = vim.api.nvim_win_get_width(props.win)
+            local line_number = vim.fn.line('w0', props.win) - 1
+            local first_line_length = vim.api.nvim_buf_get_lines(props.buf, line_number, line_number + 1, false)[1]:len()
+            local path_length = win_width - first_line_length - 10
+            if not props.focused then
+                local filename_length = vim.fn.fnamemodify(bufname, ":t"):len()
+                path_length = math.max(path_length, filename_length + 2)
+            end
+            local icon, icon_hl = require('nvim-web-devicons').get_icon(
+                vim.fn.fnamemodify(bufname, ':t'),
+                vim.fn.fnamemodify(bufname, ':e'),
+                { default = true }
+            )
+
+            local render_icon = { ' ', icon, ' ', group = icon_hl }
+            local render_path = truncate_utils(
+                (bufname ~= '' and vim.fn.fnamemodify(bufname, ':.') or '[No Name]'),
+                path_length,
+                nil,
+                -1
+            )
+            return {
+                { render_icon },
+                { render_path, gui = 'bold', guifg = vim.bo[props.buf].modified and '#ffaa00' or nil },
             }
-            return buffer
         end,
     }
 end
