e68cf51f378982f956d6d2d688647d0249059e17
momeppkt <peeranut32@gmail.com>
2024-03-24 17:16:17 +0700

    chore(nvim/trouble): remove `close_to_parent` extended action, fix has been merged

--- nvim/lua/plugins/trouble-config.lua
+++ nvim/lua/plugins/trouble-config.lua
@@ -3,24 +3,14 @@ local M = {
     cmd = 'TroubleToggle',
 }
 
-M.opts = function()
-    -- INFO: extends action to close and focus back on active split
-    local trouble = require('trouble')
-    trouble.close_to_parent = function()
-        trouble.action('cancel')
-        trouble.close()
-    end
-
-    return {
-        use_diagnostic_signs = true,
-        action_keys = {
-            close = {},
-            cancel = {},
-            close_to_parent = { 'q', '<esc>' },
-            jump = { '<tab>', '<space>' },
-            jump_close = { '<cr>', 'o' }
-        }
+M.opts = {
+    use_diagnostic_signs = true,
+    action_keys = {
+        cancel = {},
+        close = { 'q', '<esc>' },
+        jump = { '<tab>', '<space>' },
+        jump_close = { '<cr>', 'o' }
     }
-end
+}
 
 return M
